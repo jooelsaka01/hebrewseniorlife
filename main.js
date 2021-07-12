7842800333db8f99617491cef32f4984baedfe6c
jooelsaka01 <64879715+jooelsaka01@users.noreply.github.com>
2021-07-12 04:10:50 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+// wait the window to load 
+window.onload = function () {
+    // Single Line Comment
+    // document.querySelector("h1").style.color ="Purple"
+};
+
+// Ctrl + / {comment and Uncomment}
+
+// window.alert("Hello Let's Learn JS"); 
+
+// console.log {comment in Console}
+
+// console.log("Log")
+
+// console.error("Error!!");
+// console.table(["Elsaka","Youssef","osama","mohamed","ahmed"]);
+
+// console.group("Group1");
+// console.log ("Message 1");
+// console.log ("Message 2");
+
+
+// console.log("%cYoussef %cAhmed %cElsaka","color:red; font-size: 40px", "color:green; font-size: 40px; font-weight: bold;", "color:blue; font-size: 40px")
+// "%c" for selecting the text : %cWEB %cSchool
+
+// var myName = " Youssef";
+// console.log("Hello"  +  myName);
+
+// console.log(typeof {name: 'Youssef', age: 17, Country: 'EG'})
+
